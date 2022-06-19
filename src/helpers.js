c6f37af8924f93b7939ca9ea4b2cc57c5eae73fb
midoritiba <mic.tiba@gmail.com>
2022-06-19 11:32:46 +0200

    api endpoints

--- src/helpers.js
+++ src/helpers.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 export async function getCity(city){
     const API_KEY = process.env.REACT_APP_API_KEY
-    const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`)
+    const response = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`)
     const data = await response.data[0]
     return data
   } 
 
 export async function getWeather(locationKey){
     const API_KEY = process.env.REACT_APP_API_KEY
-    const response = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_KEY}`)
+    const response = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_KEY}`)
     const data = await response.data[0]
     // setWeatherDetails(data)
     return data
